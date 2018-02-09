aa8c1ff472c4c99eca2faa8b86dd101333400467
Michael Kühnel <m.kuehnel@micromata.de>
2018-02-09 12:41:06 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+module.exports = {
+	extends: [
+		'xo/esnext',
+		'plugin:import/errors',
+		'plugin:import/warnings',
+		'plugin:unicorn/recommended'
+	],
+	env: {
+		browser: true,
+		jquery: true,
+		jest: true
+	},
+	plugins: ['security', 'filenames', 'unicorn', 'import'],
+	rules: {
+		'padded-blocks': 'off',
+		'operator-assignment': 'off',
+		eqeqeq: ['error', 'allow-null'],
+		'no-eq-null': 'off',
+		'quote-props': ['error', 'as-needed'],
+		'capitalized-comments': 'warn',
+		curly: ['error', 'multi-line'],
+		'arrow-parens': 'off',
+		'no-console': 'warn',
+		'no-debugger': 'warn',
+		'filenames/match-exported': ['error', 'kebab']
+	}
+};
